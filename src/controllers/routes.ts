0f5d837556c04fcd11e6cd5b1160c9ee0a3c5f61
Mariano Goldman <mariano.goldman@decentraland.org>
2023-10-30 11:58:42 -0300

    feat: health check endpoint

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
+import { healthHandler } from './handlers/health-handler'
 import { statusHandler } from './handlers/status-handler'
 import { errorHandler } from './handlers/error-handler'
 import { staticFileHandler } from './handlers/static-file-handler'
@@ -9,6 +10,7 @@ export async function setupRouter(_globalContext: GlobalContext): Promise<Router
 
   router.use(errorHandler)
 
+  router.get('/health/live', healthHandler)
   router.get('/status', statusHandler)
   // these are necessary for hosting the wearable-preview locally
   router.get('/:file', staticFileHandler)
