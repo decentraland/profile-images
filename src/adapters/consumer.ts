2ed363e71b5268c77cc999959832e80889fe36e2
Hugo Arregui <hugo@decentraland.org>
2023-11-06 12:24:16 -0300

    restore functionality

--- src/adapters/consumer.ts
+++ src/adapters/consumer.ts
@@ -1,47 +1,53 @@
-import { SQSClient } from '@aws-sdk/client-sqs'
-import { Queue } from '../logic/queue'
-import { sleep } from '../logic/sleep'
-import { AppComponents, QueueMessage, QueueWorker } from '../types'
+import { SQSClient } from "@aws-sdk/client-sqs";
+import { Queue } from "../logic/queue";
+import { sleep } from "../logic/sleep";
+import { AppComponents, QueueMessage, QueueWorker } from "../types";
 
 export async function createConsumerComponent({
   awsConfig,
   config,
   logs,
   snapshot,
-  storage
-}: Pick<AppComponents, 'awsConfig' | 'config' | 'logs' | 'snapshot' | 'storage'>): Promise<QueueWorker> {
-  const logger = logs.getLogger('consumer')
-  const sqs = new SQSClient(awsConfig)
-  const queueName = await config.requireString('QUEUE_NAME')
-  const maxJobs = 1 //parseInt(await config.requireString('MAX_JOBS'))
-  const interval = parseInt(await config.requireString('INTERVAL'))
-  const queue = new Queue(sqs, queueName)
+  storage,
+}: Pick<
+  AppComponents,
+  "awsConfig" | "config" | "logs" | "snapshot" | "storage"
+>): Promise<QueueWorker> {
+  const logger = logs.getLogger("consumer");
+  const sqs = new SQSClient(awsConfig);
+  const queueName = await config.requireString("QUEUE_NAME");
+  const maxJobs = 1; //parseInt(await config.requireString('MAX_JOBS'))
+  const interval = parseInt(await config.requireString("INTERVAL"));
+  const queue = new Queue(sqs, queueName);
 
   const handle = async (message: QueueMessage) => {
-    logger.debug(`Processing: ${message.entity}`)
+    logger.debug(`Processing: ${message.entity}`);
 
-    // const [face, body] = await Promise.all([snapshot.getFace(message.address), snapshot.getBody(message.address)])
-    // await Promise.all([
-    //   storage.store(`entities/${message.entity}/face.png`, face),
-    //   storage.store(`entities/${message.entity}/body.png`, body)
-    // ])
-  }
+    const [face, body] = await Promise.all([
+      snapshot.getFace(message.address),
+      snapshot.getBody(message.address),
+    ]);
+    await Promise.all([
+      storage.store(`entities/${message.entity}/face.png`, face),
+      storage.store(`entities/${message.entity}/body.png`, body),
+    ]);
+  };
 
   async function job() {
-    logger.debug('Running jobs')
-    const didWork = await queue.receive(handle, maxJobs)
+    logger.debug("Running jobs");
+    const didWork = await queue.receive(handle, maxJobs);
     if (!didWork) {
-      logger.debug(`Queue empty`)
-      await sleep(interval / 2)
+      logger.debug(`Queue empty`);
+      await sleep(interval / 2);
     }
   }
 
   async function start() {
-    logger.debug('Starting consumer')
+    logger.debug("Starting consumer");
     while (true) {
-      await job()
+      await job();
     }
   }
 
-  return { start }
+  return { start };
 }
