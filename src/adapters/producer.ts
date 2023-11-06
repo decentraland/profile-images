feb0be87d136af17b3523e5f48bd5284b883a807
Hugo Arregui <hugo@decentraland.org>
2023-11-06 12:26:51 -0300

    linting

--- src/adapters/producer.ts
+++ src/adapters/producer.ts
@@ -1,75 +1,62 @@
-import { SQSClient } from "@aws-sdk/client-sqs";
-import { Queue } from "../logic/queue";
-import { AppComponents, JobProducer, QueueMessage } from "../types";
-import { sleep } from "../logic/sleep";
+import { SQSClient } from '@aws-sdk/client-sqs'
+import { Queue } from '../logic/queue'
+import { AppComponents, JobProducer, QueueMessage } from '../types'
+import { sleep } from '../logic/sleep'
 
-const LAST_CHECKED_TIMESTAMP_KEY = "last_checked_timestamp.txt";
+const LAST_CHECKED_TIMESTAMP_KEY = 'last_checked_timestamp.txt'
 
 export async function createProducerComponent({
   awsConfig,
   config,
   logs,
   profileFetcher,
-  storage,
-}: Pick<
-  AppComponents,
-  "awsConfig" | "config" | "logs" | "profileFetcher" | "storage"
->): Promise<JobProducer> {
-  const logger = logs.getLogger("producer");
-  const sqs = new SQSClient(awsConfig);
-  const queueName = await config.requireString("QUEUE_NAME");
-  const queue = new Queue(sqs, queueName);
-  const interval = parseInt(await config.requireString("INTERVAL"));
+  storage
+}: Pick<AppComponents, 'awsConfig' | 'config' | 'logs' | 'profileFetcher' | 'storage'>): Promise<JobProducer> {
+  const logger = logs.getLogger('producer')
+  const sqs = new SQSClient(awsConfig)
+  const queueName = await config.requireString('QUEUE_NAME')
+  const queue = new Queue(sqs, queueName)
+  const interval = parseInt(await config.requireString('INTERVAL'))
 
-  let lastRun = Date.now() - interval;
+  let lastRun = Date.now() - interval
 
   async function poll(ms: number, lastTimestamp: number): Promise<number> {
-    const { profiles, timestamp } = await profileFetcher.getProfilesWithChanges(
-      lastTimestamp
-    );
-    logger.debug(`Got ${profiles.length} profiles with changes`);
+    const { profiles, timestamp } = await profileFetcher.getProfilesWithChanges(lastTimestamp)
+    logger.debug(`Got ${profiles.length} profiles with changes`)
     for (const [address, entity] of profiles) {
-      const message: QueueMessage = { address, entity };
-      await queue.send(message);
-      logger.debug(
-        `Added to queue address="${address}" and entity="${entity}"`
-      );
+      const message: QueueMessage = { address, entity }
+      await queue.send(message)
+      logger.debug(`Added to queue address="${address}" and entity="${entity}"`)
     }
-    return timestamp;
+    return timestamp
   }
 
   async function changeLastRun(ts: number) {
-    lastRun = ts;
-    await storage.store(
-      LAST_CHECKED_TIMESTAMP_KEY,
-      Buffer.from(lastRun.toString())
-    );
+    lastRun = ts
+    await storage.store(LAST_CHECKED_TIMESTAMP_KEY, Buffer.from(lastRun.toString()))
   }
 
   async function start() {
-    logger.info("Starting producer");
+    logger.info('Starting producer')
 
-    const contentBuffer = await storage.retrieve(LAST_CHECKED_TIMESTAMP_KEY);
+    const contentBuffer = await storage.retrieve(LAST_CHECKED_TIMESTAMP_KEY)
     if (contentBuffer) {
-      lastRun = parseInt(contentBuffer.toString());
+      lastRun = parseInt(contentBuffer.toString())
     } else {
-      logger.info(`Could not fetch last checked timestamp.`);
+      logger.info(`Could not fetch last checked timestamp.`)
     }
-    logger.info(`Starting from ${lastRun}.`);
+    logger.info(`Starting from ${lastRun}.`)
 
     while (true) {
       try {
-        lastRun = await poll(interval, lastRun);
-        await storage.store(
-          LAST_CHECKED_TIMESTAMP_KEY,
-          Buffer.from(lastRun.toString())
-        );
+        lastRun = await poll(interval, lastRun)
+        await storage.store(LAST_CHECKED_TIMESTAMP_KEY, Buffer.from(lastRun.toString()))
       } catch (error: any) {
-        logger.error(error);
+        logger.error(error)
       }
-      await sleep(interval);
+      await sleep(interval)
     }
   }
 
-  return { start, changeLastRun };
+  return { start, changeLastRun }
 }
