c3bce5083f404ee81f4413dcf04eafea4609f13b
Hugo Arregui <hugo@decentraland.org>
2024-01-10 17:59:15 -0300

    wip

--- src/controllers/handlers/set-last-run-handler.ts
+++ src/controllers/handlers/set-last-run-handler.ts
@@ -2,14 +2,19 @@ import { HandlerContextWithPath, InvalidRequestError } from '../../types'
 import { IHttpServerComponent } from '@well-known-components/interfaces'
 
 export async function setLastRunHandler(
-  _context: HandlerContextWithPath<'jobProducer', '/set-last-run'>
+  context: HandlerContextWithPath<'jobProducer', '/set-last-run'>
 ): Promise<IHttpServerComponent.IResponse> {
-  const body = await _context.request.json()
+  const {
+    request,
+    components: { jobProducer }
+  } = context
+
+  const body = await request.json()
   if (!body || typeof body !== 'string' || !Number.isInteger(parseInt(body))) {
     throw new InvalidRequestError('Invalid request. Request body is not valid')
   }
 
-  await _context.components.jobProducer.changeLastRun(parseInt(body))
+  await jobProducer.changeLastRun(parseInt(body))
 
   return {
     status: 204
