17f7460ce06e3e5f795ca4c3ba511fc12f45be03
Hugo Arregui <hugo@decentraland.org>
2024-01-10 16:42:23 -0300

    remove health handler

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,6 +1,5 @@
 import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
-import { healthHandler } from './handlers/health-handler'
 import { statusHandler } from './handlers/status-handler'
 import { errorHandler } from './handlers/error-handler'
 import { setLastRunHandler } from './handlers/set-last-run-handler'
@@ -11,7 +10,6 @@ export async function setupRouter(_globalContext: GlobalContext): Promise<Router
   router.use(errorHandler)
 
   router.post('/set-last-run', setLastRunHandler)
-  router.get('/health/live', healthHandler)
   router.get('/status', statusHandler)
 
   return router
