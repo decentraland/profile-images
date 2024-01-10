72ebd8b8c2c52b4b3d639f700c5f4d9398f5769a
Mariano Goldman <mariano.goldman@decentraland.org>
2024-01-10 18:38:49 -0300

    fixes

--- src/controllers/handlers/set-schedule-processing-handler.ts
+++ src/controllers/handlers/set-schedule-processing-handler.ts
@@ -4,15 +4,19 @@ import { SQSClient } from '@aws-sdk/client-sqs'
 import { Queue } from '../../logic/queue'
 
 export async function scheduleProcessingHandler(
-  _context: HandlerContextWithPath<
+  context: HandlerContextWithPath<
     'awsConfig' | 'config' | 'jobProducer' | 'logs' | 'profileFetcher',
     '/schedule-processing'
   >
 ): Promise<IHttpServerComponent.IResponse> {
-  const { awsConfig, config, logs } = _context.components
+  const {
+    request,
+    components: { awsConfig, config, logs }
+  } = context
+
   const logger = logs.getLogger('schedule-processing-handler')
 
-  const body = await _context.request.json()
+  const body = await request.json()
   console.log('body', body)
   if (!body || typeof body !== 'object' || !Array.isArray(body)) {
     throw new InvalidRequestError('Invalid request. Request body is not valid')
@@ -22,7 +26,6 @@ export async function scheduleProcessingHandler(
   const queueName = await config.requireString('QUEUE_NAME')
   const queue = new Queue(sqs, queueName)
 
-  logger.debug(`Got ${body.length} profiles with changes`)
   for (const entity of body) {
     const message: QueueMessage = { entity }
     await queue.send(message)
