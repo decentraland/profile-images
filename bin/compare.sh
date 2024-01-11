9bfb3970519fb2d449f3e8309f34a9eb403e0a9a
Hugo Arregui <hugo@decentraland.org>
2024-01-11 10:48:47 -0300

    change filename so it's easier to compare them

--- bin/compare.sh
+++ bin/compare.sh
@@ -7,21 +7,20 @@ rm -r compare
 mkdir -p compare
 
 while IFS= read -r entity; do
-  wget -q $BUCKET/entities/${entity}/face.png -O compare/${entity}_godot_face.png
+  wget -q $BUCKET/entities/${entity}/face.png -O compare/${entity}_face_godot.png
   if [ $? -eq 8 ]; then
     echo "${entity} is not yet processed"
-    rm compare/${entity}_godot_face.png
+    rm compare/${entity}_face_godot.png
     continue
   fi
-  wget -q $BUCKET/entities/${entity}/body.png -O compare/${entity}_godot_body.png
+  wget -q $BUCKET/entities/${entity}/body.png -O compare/${entity}_body_godot.png
 
   wget -q $CONTENT/contents/${entity} -O compare/${entity}-profile.json
-
   BODY_HASH=$(cat compare/${entity}-profile.json | jq '.content[] | select(.file == "body.png") | .hash' | cut -d\" -f2)
-  FACE_HASH=$(cat compare/${entity}-profile.json | jq '.content[] | select(.file == "face.png") | .hash' | cut -d\" -f2)
+  FACE_HASH=$(cat compare/${entity}-profile.json | jq '.content[] | select(.file == "face256.png") | .hash' | cut -d\" -f2)
 
-  wget -q $CONTENT/contents/${BODY_HASH} -O compare/${entity}_content_body.png
-  wget -q $CONTENT/contents/${FACE_HASH} -O compare/${entity}_content_face.png
+  wget -q $CONTENT/contents/${BODY_HASH} -O compare/${entity}_body_content.png
+  wget -q $CONTENT/contents/${FACE_HASH} -O compare/${entity}_face_content.png
 
   rm compare/${entity}-profile.json
 done
