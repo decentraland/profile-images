8f221acd771ef8ccccab7ececede8a7c56f29f41
Hugo Arregui <hugo@decentraland.org>
2024-01-11 14:35:42 -0300

    Save Thu Jan 11 02:35:42 PM -03 2024

--- src/adapters/godot.ts
+++ src/adapters/godot.ts
@@ -5,14 +5,6 @@ import path from 'path'
 import { AppComponents, AvatarGenerationResult, GodotComponent } from '../types'
 import { Entity } from '@dcl/schemas'
 
-type OptionsGenerateAvatars = Partial<{
-  outputPath: string
-  faceWidth: number
-  faceHeight: number
-  width: number
-  height: number
-}>
-
 type GodotAvatarPayload = {
   entity: string
   destPath: string
@@ -35,6 +27,12 @@ export function splitUrnAndTokenId(urnReceived: string) {
     return { urn: urnReceived, tokenId: undefined }
   }
 }
+
+const outputPath = 'output'
+const width = 256
+const height = 512
+const faceWidth = 256
+const faceHeight = 256
 
 const profileWithAssetUrns = (profile: any) => {
   return {
@@ -67,98 +65,100 @@ export async function createGodotSnapshotComponent({
 
   let executionNumber = 0
 
-  function run(entities: string[], options: OptionsGenerateAvatars): Promise<AvatarGenerationResult[]> {
+  function run(input: any): Promise<void> {
     return new Promise(async (resolve, reject) => {
-      logger.debug(`Running godot to process ${entities.length}: ${JSON.stringify(entities)}`)
       // unique number for temp files
       executionNumber += 1
 
       // create directory if exists
-      if (options.outputPath && !existsSync(options.outputPath)) {
-        mkdirSync(options.outputPath)
+      if (!existsSync(outputPath)) {
+        mkdirSync(outputPath)
       }
 
-      const response = await fetch.fetch(`${peerUrl}/content/entities/active`, {
-        method: 'POST',
-        body: JSON.stringify({ ids: entities })
-      })
-      const profiles: Entity[] = (await response.json()).map(profileWithAssetUrns)
-
-      const payloads: GodotAvatarPayload[] = []
-      const results: AvatarGenerationResult[] = []
-
-      for (const entity of entities) {
-        const profile = profiles.find((p) => p.id === entity)
-        const destPath = path.join(options.outputPath ?? '', `${entity}.png`)
-        const faceDestPath = path.join(options.outputPath ?? '', `${entity}_face.png`)
-        if (profile) {
-          payloads.push({
-            entity,
-            destPath,
-            width: options.width,
-            height: options.height,
-            faceDestPath,
-            faceWidth: options.faceWidth,
-            faceHeight: options.faceHeight,
-            avatar: profile.metadata.avatars[0].avatar
-          })
-        }
-        results.push({
-          status: false,
-          entity,
-          avatarPath: destPath,
-          facePath: faceDestPath
-        })
-      }
-
-      if (payloads.length === 0) {
-        return resolve(results)
-      }
-
-      const output = {
-        baseUrl: `${peerUrl}/content`,
-        payload: payloads
-      }
       // logger.debug(`output: ${JSON.stringify(output)}`)
 
       const avatarDataPath = `temp-avatars-${executionNumber}.json`
 
-      await writeFile(avatarDataPath, JSON.stringify(output))
+      await writeFile(avatarDataPath, JSON.stringify(input))
       const command = `${explorerPath}/decentraland.godot.client.x86_64 --rendering-driver opengl3 --avatar-renderer --avatars ${avatarDataPath}`
       logger.debug(`about to exec, explorerPath: ${explorerPath}, display: ${process.env.DISPLAY}, command: ${command}`)
 
-      const timer = metrics.startTimer('snapshot_generation_duration_seconds', { profiles: payloads.length })
-      console.time(`screenshots for ${entities.length} entities`)
       exec(command, { timeout: 30_000 }, (error, _stdout, _stderr) => {
         rmSync(avatarDataPath)
 
         if (error) {
-          timer.end({ status: 'error' })
-          console.timeEnd(`screenshots for ${entities.length} entities`)
           return reject(error)
         }
 
-        for (const result of results) {
-          if (existsSync(result.avatarPath) && existsSync(result.facePath)) {
-            result.status = true
-          }
-        }
-
-        console.timeEnd(`screenshots for ${entities.length} entities`)
-        timer.end({ status: 'success' })
-        resolve(results)
+        resolve()
       })
     })
   }
 
   async function generateImages(entities: string[]): Promise<AvatarGenerationResult[]> {
-    return await run(entities, {
-      outputPath: 'output',
-      width: 256,
-      height: 512,
-      faceWidth: 256,
-      faceHeight: 256
+    const response = await fetch.fetch(`${peerUrl}/content/entities/active`, {
+      method: 'POST',
+      body: JSON.stringify({ ids: entities })
     })
+    const profiles: Entity[] = (await response.json()).map(profileWithAssetUrns)
+
+    const payloads: GodotAvatarPayload[] = []
+    const results: AvatarGenerationResult[] = []
+
+    for (const entity of entities) {
+      const profile = profiles.find((p) => p.id === entity)
+      const destPath = path.join(outputPath, `${entity}.png`)
+      const faceDestPath = path.join(outputPath, `${entity}_face.png`)
+      if (profile) {
+        payloads.push({
+          entity,
+          destPath,
+          width,
+          height,
+          faceDestPath,
+          faceWidth,
+          faceHeight,
+          avatar: profile.metadata.avatars[0].avatar
+        })
+      }
+      results.push({
+        status: false,
+        entity,
+        avatarPath: destPath,
+        facePath: faceDestPath
+      })
+    }
+
+    if (payloads.length === 0) {
+      return results
+    }
+
+    const input = {
+      baseUrl: `${peerUrl}/content`,
+      payload: payloads
+    }
+
+    logger.debug(`Running godot to process ${payloads.length}: ${JSON.stringify(input)}`)
+
+    const timer = metrics.startTimer('snapshot_generation_duration_seconds', { profiles: payloads.length })
+    console.time(`screenshots for ${payloads.length} entities`)
+    try {
+      await run(input)
+      timer.end({ status: 'success' })
+      console.timeEnd(`screenshots for ${payloads.length} entities`)
+
+      for (const result of results) {
+        if (existsSync(result.avatarPath) && existsSync(result.facePath)) {
+          result.status = true
+        }
+      }
+
+      return results
+    } catch (err) {
+      timer.end({ status: 'error' })
+      console.timeEnd(`screenshots for ${payloads.length} entities`)
+      throw err
+    }
   }
 
   return {
