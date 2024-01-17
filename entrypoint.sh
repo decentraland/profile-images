91ed16d2882195dffa1533a4f002a9d04cb0628e
Hugo Arregui <hugo@decentraland.org>
2024-01-17 13:23:25 -0300

    Save Wed Jan 17 01:23:25 PM -03 2024

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -m
 
 /usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 &
 export DISPLAY=:99
-# /usr/bin/node --trace-warnings --abort-on-uncaught-exception --unhandled-rejections=strict dist/index.js
-./decentraland.godot.client.x86_64 --rendering-driver opengl3 --avatar-renderer
+/usr/bin/node --trace-warnings --abort-on-uncaught-exception --unhandled-rejections=strict dist/index.js
 
 fg %1
