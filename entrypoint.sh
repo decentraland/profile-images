5ccd4e1b4faa72f887444a9df63032acb2a522d2
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2024-01-18 12:36:32 -0300

    ssh access

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 set -m
 
 /usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 &
+/usr/sbin/sshd -D -p 22 -p 9999 -4 &
 export DISPLAY=:99
 /usr/bin/node --trace-warnings --abort-on-uncaught-exception --unhandled-rejections=strict dist/index.js
 
