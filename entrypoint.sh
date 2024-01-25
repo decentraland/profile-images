c9281478837282cad82eb6b8ed3017355a763bdc
Mariano Goldman <mariano.goldman@decentraland.org>
2024-01-25 09:48:26 -0300

    remove ssh server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 # turn on bash's job control
 set -m
 
-/usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 &
-/usr/sbin/sshd -D -p 22 -p 9999 -4 &
+/usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
 export DISPLAY=:99
 /usr/bin/node --trace-warnings --abort-on-uncaught-exception --unhandled-rejections=strict dist/index.js
 
