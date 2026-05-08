17180282b437060a101f402f862f57d982df3316
Mateo Miccino <mateomiccino@gmail.com>
2026-05-08 17:36:51 -0300

    fix: disable core dumps in entrypoint to prevent disk fill

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # turn on bash's job control
 set -m
 
+# Disable core dumps (godot writes ~348MB cores per run, fills Fargate disk).
+ulimit -c 0
+
 /usr/bin/Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
 export DISPLAY=:99
 /usr/bin/node --trace-warnings --abort-on-uncaught-exception --unhandled-rejections=strict dist/index.js
